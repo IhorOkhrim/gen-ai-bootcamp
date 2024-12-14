d347c647dac18e63ec957c7399c445a07658b362
IhorOkhrim <122023022+IhorOkhrim@users.noreply.github.com>
2024-12-14 22:35:14 +0200

    Feature/module3 (#3)

--- tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/model/Prompt.java
+++ tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/model/Prompt.java
@@ -1,9 +1,26 @@
 package com.epam.training.gen.ai.model;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Size;
+import javax.annotation.Nullable;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class Prompt {
 
-    private String input;
+    @NonNull
+    @Size(min = 1, message = "Input must not be empty")
+    private final String input;
+
+    @Nullable
+    @DecimalMin(value = "0.0", message = "Value must be greater than or equal to 0.0")
+    @DecimalMax(value = "2.0", message = "Value must be less than or equal to 2.0")
+    private Double temperature;
+
+    private String modelName;
+
+    private boolean functionsEnabled;
+
 }
