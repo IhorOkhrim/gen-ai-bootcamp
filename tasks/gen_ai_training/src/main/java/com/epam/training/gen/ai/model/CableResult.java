93e9882bc8dfd71e625d010285a8e1a8e2745c59
IhorOkhrim <122023022+IhorOkhrim@users.noreply.github.com>
2024-12-21 22:35:49 +0200

    module4 - add model and plugin (WIP) (#4)
    * module4 - add model and plugin
    * module4 - tune and fix

---
+++ tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/model/CableResult.java
@@ -0,0 +1,9 @@
+package com.epam.training.gen.ai.model;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CableResult {
+    public double voltageDrop; // in volts
+    public double resistance; // in ohms
+}
